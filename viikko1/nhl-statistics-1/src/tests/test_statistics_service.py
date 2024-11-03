881698717bf0f0d99d081511e3c2950cffcd6fc4
pronom8 <mangros19@gmail.com>
2024-11-03 11:41:48 +0200

    s

--- viikko1/nhl-statistics-1/src/tests/test_statistics_service.py
+++ viikko1/nhl-statistics-1/src/tests/test_statistics_service.py
@@ -2,51 +2,50 @@ import unittest
 from statistics_service import StatisticsService
 from player import Player
 
-
-class StubPlayerReader:
+class PlayerReaderStub:
     def get_players(self):
-        
         return [
-            Player("Wayne Gretzky", "EDM", 894, 1963),
-            Player("Mario Lemieux", "PIT", 690, 1723),
-            Player("Jaromir Jagr", "PIT", 766, 1921),
-            Player("Mark Messier", "EDM", 694, 1887),
-            Player("Paul Coffey", "EDM", 396, 1531),
-            Player("Bobby Orr", "BOS", 270, 915),
-            Player("Sidney Crosby", "PIT", 550, 1500),
-            Player("Alex Ovechkin", "WSH", 730, 1390),
-            Player("Connor McDavid", "EDM", 251, 850),
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri", "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
         ]
 
 class TestStatisticsService(unittest.TestCase):
     def setUp(self):
-        
-        reader_stub = StubPlayerReader()
-        self.statistics = StatisticsService(reader_stub)
+        # injektoi PlayerReaderStub StatisticsService-luokkaan
+        self.stats = StatisticsService(PlayerReaderStub())
 
-    def test_search_finds_existing_player(self):
-        player = self.statistics.search("Wayne Gretzky")
+    def test_search_finds_player(self):
+        player = self.stats.search("Gretzky")
         self.assertIsNotNone(player)
-        self.assertEqual(player.name, "Wayne Gretzky")
+        self.assertEqual(player.name, "Gretzky")
 
-    def test_search_returns_none_for_nonexistent_player(self):
-        player = self.statistics.search("Nonexistent Player")
+    def test_search_returns_none_if_player_not_found(self):
+        player = self.stats.search("Nonexistent")
         self.assertIsNone(player)
 
     def test_team_returns_correct_players(self):
-        team_players = self.statistics.team("EDM")
-        team_names = [player.name for player in team_players]
-        self.assertIn("Wayne Gretzky", team_names)
-        self.assertIn("Mark Messier", team_names)
-        self.assertIn("Paul Coffey", team_names)
-        self.assertEqual(len(team_players), 4)  
+        team_players = self.stats.team("EDM")
+        self.assertEqual(len(team_players), 3)  # EDM:llä on kolme pelaajaa
+        self.assertEqual(team_players[0].name, "Semenko")
+        self.assertEqual(team_players[1].name, "Kurri")
+        self.assertEqual(team_players[2].name, "Gretzky")
 
-    def test_top_returns_correct_number_of_players(self):
-        top_players = self.statistics.top(3)
+    def test_top_returns_top_players(self):
+        top_players = self.stats.top(2)  # Palautetaan 3 parasta (how_many + 1)
         self.assertEqual(len(top_players), 3)
+        self.assertEqual(top_players[0].name, "Gretzky")  # Paras pelaaja
+        self.assertEqual(top_players[1].name, "Lemieux")  # Toinen paras
+        self.assertEqual(top_players[2].name, "Yzerman")  # Kolmas paras
 
-    def test_top_returns_players_sorted_by_points(self):
-        top_players = self.statistics.top(3)
-        self.assertEqual(top_players[0].name, "Wayne Gretzky")
-        self.assertEqual(top_players[1].name, "Mario Lemieux")
-        self.assertEqual(top_players[2].name, "Jaromir Jagr")
+    def test_top_does_not_fail_with_zero(self):
+        top_players = self.stats.top(0)
+        self.assertEqual(len(top_players), 1)  # Vain paras pelaaja
+        self.assertEqual(top_players[0].name, "Gretzky")
+
+    def test_top_does_not_fail_if_how_many_is_greater_than_list_size(self):
+        top_players = self.stats.top(10)  # Pelaajia on vain 5
+        self.assertEqual(len(top_players), 5)
+
