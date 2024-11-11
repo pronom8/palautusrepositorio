61b597637028e0b46e33951dc635b519f9cb8f1d
pronom8 <mangros19@gmail.com>
2024-11-11 17:56:18 +0200

    player class done

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
     def __init__(self, dict):
-        self.name = dict['name']
+        self.name = dict.get('name', 'N/A')
+        self.team = dict.get('team', 'N/A')
+        self.goals = dict.get('goals', 0)
+        self.assists = dict.get('assists', 0)
+        self.nationality = dict.get('nationality', 'N/A')
+
     
     def __str__(self):
-        return self.name
+        return f"{self.name} team {self.team}  goals {self.goals} assists {self.assists}"
+
