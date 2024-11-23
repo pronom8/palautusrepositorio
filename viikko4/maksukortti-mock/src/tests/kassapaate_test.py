a3f26efcc90152543cc0fad4e5996b88a58eb93f
pronom8 <mangros19@gmail.com>
2024-11-23 13:36:12 +0200

    week 4, t2 done

--- viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,20 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_lataa(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 20
+
+        self.kassa.lataa(maksukortti_mock, 30)
+
+        maksukortti_mock.lataa.assert_called_with(30)
+
+    def test_lataa_negatiivista_summaa(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo.return_value = 20
+
+        self.kassa.lataa(maksukortti_mock, -30)
+
+        maksukortti_mock.lataa.assert_not_called()
+
